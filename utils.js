74303826b838a2c6f449dbbb4a446fbf7e99d81e
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-10-06 13:11:57 +0100

    feat: implement mail sender with nodemailer

--- utils.js
+++ utils.js
@@ -1,4 +1,8 @@
-async function getMailInfo(requestObject) {
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config()
+
+function getMailInfo(requestObject) {
     return new Promise((resolve, reject) => {
         let mailInfo = []
         requestObject.on('data', chunk => {
@@ -11,4 +15,69 @@ async function getMailInfo(requestObject) {
     })
 }
 
-export { getMailInfo };
+function sendMail(sender, to, subject, body, htmlBody) {
+    return new Promise((resolve, reject) => {
+        nodemailer.createTestAccount((err, account) => {
+            if (err) {
+                console.error('Failed to create a testing account');
+                console.error(err);
+                return process.exit(1);
+            }
+
+            console.log('Credentials obtained, sending message...', sender);
+
+            // NB! Store the account object values somewhere if you want
+            // to re-use the same account for future mail deliveries
+
+            // Create a SMTP transporter object
+            let transporter = nodemailer.createTransport(
+                {
+                    service: "gmail",
+                    auth: {
+                        user: process.env.MAIL_USER,
+                        pass: process.env.MAIL_PASS
+                    },
+                    logger: true,
+                    allowInternalNetworkInterfaces: false
+                },
+                {
+                    // default message fields
+
+                    // sender info
+                    from:  `From-${sender||to} <${process.env.MAIL_USER}>`,
+                }
+            );
+
+            // Message object
+            let message = {
+                // Comma separated list of recipients
+                to: to,
+
+                // Subject of the message
+                subject: subject + " --- " + Date.now(),
+
+                // plaintext body
+                text: body,
+
+                // HTML body
+                html: htmlBody,
+            };
+
+            transporter.sendMail(message, (error, status) => {
+                if (error) {
+                    console.log('Error occurred');
+                    console.log(error.message);
+                    reject({success: false, status: error})
+                }
+
+                resolve({success: true, status})
+                console.log(nodemailer.getTestMessageUrl(success));
+
+                // only needed when using pooled connections
+                transporter.close();
+            });
+        });
+    })
+}
+
+export { getMailInfo, sendMail };

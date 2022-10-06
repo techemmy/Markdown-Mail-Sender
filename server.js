78fc022720eca9646454cc31595cc42a61a14daf
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-10-06 13:18:02 +0100

    feat: request handler for request to send mail and handle static files
    Introduce pagedown to parse markdown recieved from frontend as HTML and pass it to the mail sender
    . Handle error handling for promise returned by sendMail() function
    
    Implement proper handling of static files request

--- server.js
+++ server.js
@@ -2,34 +2,42 @@ import { createServer } from "http";
 import { getSanitizingConverter } from "pagedown";
 import fs from "fs";
 import dotenv from "dotenv";
-import { getMailInfo } from "./utils.js";
+import { getMailInfo, sendMail } from "./utils.js";
 
 dotenv.config();
 const HOST = process.env.HOST;
 const PORT = process.env.PORT;
 
 const app = createServer(async (req, res) => {
-
-    // load public files
-    if (req.url.startsWith("/public")) {
-        console.log("ok", `.${req.url}`)
-        var file = fs.createReadStream(`.${req.url}`);
-        file.pipe(res);
-        res.writeHead(200, {'Content-Type': 'text/javascript'});
-    }
-
     // render html files to response
     let pathToFile = "./views/";
     switch (req.url) {
         case "/":
             pathToFile += "index.html";
             if (req.method === "POST") {
-                const {mailTo, mailSubject, mailContent} = await getMailInfo(req);
-                console.log(mailTo, mailSubject, mailContent);
+                try {
+                    const {mailFrom, mailTo, mailSubject, mailBody} = await getMailInfo(req);
+                    const markdownToHTMLConverter = new getSanitizingConverter();
+                    const mailBodyInHTML = markdownToHTMLConverter.makeHtml(mailBody)
+                    const mailResponse = await sendMail(mailFrom, mailTo, mailSubject, mailBody, mailBodyInHTML);
+                    res.writeHead(200, {'Content-Type':'application/json'})
+                    res.end(JSON.stringify(mailResponse))
+                } catch (error) {
+                    res.writeHead(500, {'Content-Type':'application/json'})
+                    res.end(JSON.stringify(error))
+                }
             }
             break;
 
         default:
+            // load public files
+            if (req.url.startsWith("/public")) {
+                console.log("ok", `.${req.url}`)
+                var file = fs.createReadStream(`.${req.url}`);
+                file.pipe(res);
+                res.writeHead(200, {'Content-Type': 'text/javascript'});
+            }
+
             pathToFile += "404.html";
             break
     }

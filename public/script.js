003f07e942621d7c70d2979c4eb514bfa01c2849
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-10-07 05:00:36 +0100

    fix: handle validating user's input before they continue sending a mail

--- public/script.js
+++ public/script.js
@@ -1,5 +1,9 @@
 const sendBtn = document.getElementById('send-btn');
 const mailBodyContainer = document.getElementById("mail-body");
+const mailFrom = document.getElementById('username')
+const mailTo = document.getElementById('receiver-email')
+const mailSubject = document.getElementById('mail-subject')
+
 
 var simplemde = new SimpleMDE({
     element: mailBodyContainer, autosave: {
@@ -29,22 +33,28 @@ sendBtn.addEventListener('click', event => {
         event.preventDefault();
 
         const mailBody = simplemde.value().trim();
-        if (mailBody.length > 10) {
-            fetch("/", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json; charset=UTF-8"
-                },
-                body: JSON.stringify({
-                    mailFrom: "TechEmmy",
-                    mailTo: "ywwmrvzuo@emergentvillage.org",
-                    mailSubject: "Testing mailer",
-                    mailBody
-                })
-            }).then(async data => {
-                console.log("Fetched", await data.json());
-            }).catch(error => {
-                alert("An error occured, cound't send data")
-            })
+        if (mailBody.length < 1 || mailFrom.value.length < 1 || mailTo.value.length < 1 || mailSubject.value.length < 1) {
+            alert("Make sure all fields are properly filled.")
+            return
         }
+
+        sendBtn.style.display = "none";
+        fetch("/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=UTF-8"
+            },
+            body: JSON.stringify({
+                mailFrom: mailFrom.value,
+                mailTo: mailTo.value.split(","),
+                mailSubject: mailSubject.value,
+                mailBody
+            })
+        }).then(async data => {
+            console.log("Fetched", await data.json());
+            sendBtn.style.display = "block";
+        }).catch(error => {
+            alert("An error occured, cound't send data")
+            sendBtn.style.display = "block";
+        })
     });

45372e706169bcdd0eca18296edafa5cab396010
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-10-07 02:48:40 +0100

    fix: reimplement the file serving mechanism

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import http from "http";
 import { getSanitizingConverter } from "pagedown";
-import fs from "fs";
 import dotenv from "dotenv";
-import { getMailInfo, sendMail } from "./utils.js";
+import { getMailInfo, sendMail, serveFile } from "./utils.js";
 import statik from "node-static";
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -16,53 +15,44 @@ const __dirname = path.dirname(__filename);
 var fileServer = new statik.Server(__dirname);
 
 const app = http.createServer(async (req, res) => {
-
-    // render html files to response
     let pathToFile = "./views/";
-    switch (req.url) {
-        case "/":
-            pathToFile += "index.html";
-            if (req.method === "POST") {
-                try {
-                    const {mailFrom, mailTo, mailSubject, mailBody} = await getMailInfo(req);
-                    const markdownToHTMLConverter = new getSanitizingConverter();
-                    const mailBodyInHTML = markdownToHTMLConverter.makeHtml(mailBody)
-                    const mailResponse = await sendMail(mailFrom, mailTo, mailSubject, mailBody, mailBodyInHTML);
-                    res.writeHead(200, {'Content-Type':'application/json'})
-                    res.end(JSON.stringify(mailResponse))
-                } catch (error) {
-                    res.writeHead(500, {'Content-Type':'application/json'})
-                    res.end(JSON.stringify(error))
+
+    if (req.url === "/" && req.method === "GET") {
+        pathToFile += "index.html";
+        serveFile(res, pathToFile)
+    } else if (req.method === "POST") {
+        try {
+            const {mailFrom, mailTo, mailSubject, mailBody} = await getMailInfo(req);
+            const markdownToHTMLConverter = new getSanitizingConverter();
+            const mailBodyInHTML = markdownToHTMLConverter.makeHtml(mailBody)
+            const mailResponse = await sendMail(mailFrom, mailTo, mailSubject, mailBody, mailBodyInHTML);
+            res.writeHead(200, {'Content-Type':'application/json'})
+            res.end(JSON.stringify(mailResponse))
+        } catch (error) {
+            res.writeHead(500, {'Content-Type':'application/json'})
+            res.end(JSON.stringify(error))
+        }
+
+    } else if (req.url.startsWith("/public")) {
+        req.addListener('end', function () {
+            fileServer.serve(req, res, function (err, result) {
+                if (err) { // There was an error serving the file
+                    console.error(
+                        "Error serving " + req.url + " - " + err.message
+                    );
+
+                    // Respond to the client
+                    res.writeHead(err.status, err.headers);
+                    res.end();
                 }
-            }
-            break;
-
-        default:
-            req.addListener('end', function () {
-                fileServer.serve(req, res, function (err, result) {
-                    if (err) { // There was an error serving the file
-                        console.error(
-                            "Error serving " + req.url + " - " + err.message
-                        );
-
-                        // Respond to the client
-                        res.writeHead(err.status, err.headers);
-                        res.end();
-                    }
-                });
-            }).resume();
-
-            pathToFile += "404.html";
-            break
+            });
+        }).resume();
+    } else {
+        pathToFile += "404.html";
+        serveFile(res, pathToFile)
     }
 
-    fs.readFile(pathToFile, (err, file) => {
-        if (err) {
-            console.log(err);
-            res.end("An error occured");
-        }
-        res.end(file);
-    });
+
 });
 
 app.listen(PORT, HOST, () => {

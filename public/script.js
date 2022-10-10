7114fcaf490595f8ac2dcb20e7cbd8fd6f066708
Emmanuel Oloyede <emmanueltopea@gmail.com>
2022-10-10 13:55:09 +0100

    feat: add validation to prevent user from sending mail for more than 3 times

--- public/script.js
+++ public/script.js
@@ -1,8 +1,12 @@
 const sendBtn = document.getElementById('send-btn');
 const mailBodyContainer = document.getElementById("mail-body");
-const mailFrom = document.getElementById('username')
+const username = document.getElementById('username')
 const mailTo = document.getElementById('receiver-email')
 const mailSubject = document.getElementById('mail-subject')
+const maxNoOfMailText = document.getElementById('max-no-of-trials')
+const MAX_NO_OF_EMAIL_SEND_TRIALS = 3
+// const senderEmail = document.getElementById('sender-email')
+// const senderPassword = document.getElementById('sender-password')
 
 
 var simplemde = new SimpleMDE({
@@ -14,47 +18,82 @@ var simplemde = new SimpleMDE({
 });
 
 if (simplemde.value().trim().length > 1) {
-       sendBtn.disabled = false;
-    } else {
-        sendBtn.disabled = true;
-    }
+    sendBtn.disabled = false;
+} else {
+    sendBtn.disabled = true;
+}
 
-simplemde.codemirror.on("change", function(){
+simplemde.codemirror.on("change", function () {
     const mailBody = simplemde.value().trim();
 
     if (mailBody.length > 1) {
-       sendBtn.disabled = false;
+        sendBtn.disabled = false;
     } else {
         sendBtn.disabled = true;
     }
 });
 
 sendBtn.addEventListener('click', event => {
-        event.preventDefault();
+    event.preventDefault();
 
-        const mailBody = simplemde.value().trim();
-        if (mailBody.length < 1 || mailFrom.value.length < 1 || mailTo.value.length < 1 || mailSubject.value.length < 1) {
-            alert("Make sure all fields are properly filled.")
-            return
+    const mailBody = simplemde.value().trim();
+    if (mailBody.length < 1 || username.value.length < 1 || mailTo.value.length < 1 || mailSubject.value.length < 1) {
+        alert("Make sure all fields are properly filled.")
+        return
+    }
+
+    if (localStorage.getItem('emailsSent') > MAX_NO_OF_EMAIL_SEND_TRIALS) {
+        alert("Maximum no of emails sent reached!");
+        return
+    }
+
+    sendBtn.textContent = "Sending...";
+    sendBtn.disabled = true;
+
+    fetch("/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify({
+            username: username.value,
+            mailTo: mailTo.value.split(","),
+            mailSubject: mailSubject.value,
+            mailBody,
+            // senderEmail: senderEmail.value,
+            // senderPassword: senderPassword.value
+        })
+    }).then(async data => {
+
+        const mailsSent = Number(localStorage.getItem('emailsSent'));
+        if (!mailsSent) {
+            localStorage.setItem('emailsSent', 1)
+        } else {
+            localStorage.setItem('emailsSent', mailsSent + 1)
         }
 
-        sendBtn.style.display = "none";
-        fetch("/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json; charset=UTF-8"
-            },
-            body: JSON.stringify({
-                mailFrom: mailFrom.value,
-                mailTo: mailTo.value.split(","),
-                mailSubject: mailSubject.value,
-                mailBody
-            })
-        }).then(async data => {
-            console.log("Fetched", await data.json());
-            sendBtn.style.display = "block";
-        }).catch(error => {
-            alert("An error occured, cound't send data")
-            sendBtn.style.display = "block";
-        })
-    });
+        const response = await data.json();
+        console.log("Fetched", response);
+        maxNoOfMailText.textContent = `${MAX_NO_OF_EMAIL_SEND_TRIALS - mailsSent}`;
+        if (response.success) {
+            console.log("success")
+            sendBtn.textContent = "Sent...";
+            setTimeout(() => {
+                sendBtn.disabled = true;
+            }, 1500);
+        } else {
+            console.log("failure")
+            sendBtn.textContent = "Error sending mail";
+            sendBtn.style.backgroundColor = "#FFCCCC";
+            setTimeout(() => {
+                sendBtn.style.backgroundColor = "#92E3A9";
+                sendBtn.textContent = "Send";
+                sendBtn.disabled = false;
+            }, 2000);
+        }
+
+    }).catch(error => {
+        alert("An error occured", error.response)
+        sendBtn.disabled = false;
+    })
+});
